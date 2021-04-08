93c7e861da46d580a74a8e31056fcbcd7c745c77
Guadalupe Rosas <76502431+guadalupe182@users.noreply.github.com>
2021-04-08 10:49:17 -0500

    add files

---
+++ main.js
@@ -0,0 +1,15 @@
+function yes(){
+    alert('thank you (kiss, kiss) send place, time and date <3'); 
+}
+function not(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    newWidth = Math.random() * width;
+    newHeigth = Math.random() * height;
+
+    document.getElementById('btnNo').style.position = "absolute";
+    document.getElementById('btnNo').style.left = newWidth + "px";
+    document.getElementById('btnNo').style.top = newHeigth + "px";
+
+}
